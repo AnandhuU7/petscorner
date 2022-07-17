69e9acc072194bc73790108dfa112ac4b6b8e54a
lijovijayan <lijovijayan00@gmail.com>
2022-07-17 18:31:07 +0530

    added footer

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "../../components/footer";
 import { PetCard } from "../../components/pet-card";
 import { DOGS } from "../../constants/dogs";
 import { HomeCarousel } from "./carousel";
@@ -14,6 +15,7 @@ export function HomePage() {
             <Recommendations />
             <TopSelections />
             <ExploreMore />
+            <Footer />
         </div>
     );
 }
